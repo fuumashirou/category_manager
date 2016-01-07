689f723b287be7dbba93ebe22a766195840c2ff6
fuumashirou <s.matiassalgado@gmail.com>
2016-01-06 22:56:35 -0300

    WIP: rutas para la API, aun falta corregir errores

---
+++ app/controllers/v1/categories_controller.rb
@@ -0,0 +1,29 @@
+module V1
+	class CategoriesController < ApplicationController
+
+		def index
+			@category = Category.all
+			render json: @categories
+		end
+
+		def create
+		  @category = Category.new(category_params)
+
+		  if @category.save
+		    render json: @category
+		  else
+		    render json: @category.errors, status: :unprocessable_entity
+		  end
+		end
+
+		def show
+			@category = Category.find(params[:id])
+  	end
+
+		private
+
+		def category_params
+			params.require(:category).permit(:name, :parent_id)
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  namespace :v1 do
+    resources :categories
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
